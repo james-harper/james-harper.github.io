f6ae6f579d56badc6d626369e83094f33be150d6
James Harper <jameswilliamharper@gmail.com>
2017-08-12 17:12:06 +0100

    Add comments to utility functions

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,8 +1,16 @@
 import _deburr from 'lodash/deburr';
 
-const clean = string => {
+/**
+ * @module utils/string String utility methods
+ */
+
+ /**
+  * Clean a string up for better comparisons when searching
+  *
+  * @param {string} string - The string that requires cleaning
+  * @returns {string} The cleaned string
+  */
+export function clean(string) {
   let s = _deburr(string);
   return s.toLowerCase();
 }
-
-export {clean};
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,12 @@
+/**
+ * @module utils/url Url utility methods
+ */
+
 const url = {};
 
+/**
+ * Update the URL to reflect the current state of the application
+ */
 url.update = function() {
   let url = window.location.origin + window.location.pathname;
   let toAppend = [];
@@ -14,6 +21,11 @@ url.update = function() {
   history.replaceState({}, document.title, url);
 };
 
+/**
+ * Turn url query string from a string to an array of key-value pairs
+ *
+ * @returns {Array} key-value pairs found in the query string
+ */
 url.decodeQueryString = function() {
   let queryString = location.search&&location.search.substr(1).replace(/\+/gi," ").split("&");
   for (let i in queryString) {
